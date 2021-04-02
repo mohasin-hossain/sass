cfc4df0e30d1870ad54ac1c794f56621288d93c9
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2021-04-02 18:35:05 +0600

    Responsive Design

---
+++ script.js
@@ -0,0 +1,14 @@
+// Mobile Navigation Toggler
+
+const mobileBtn = document.getElementById('mobile-cta');
+const nav = document.querySelector('nav');
+const mobileBtnExit = document.getElementById('mobile-exit');
+
+
+mobileBtn.addEventListener('click', () => {
+    nav.classList.add('menu-btn');
+})
+
+mobileBtnExit.addEventListener('click', () => {
+    nav.classList.remove('menu-btn');
+})
